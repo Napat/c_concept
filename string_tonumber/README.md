eb1d5e3dc5c71368111858f4751c5dc3c4f4ca48
Napat Rungruangbangchan <napat.joe@gmail.com>
2017-09-09 01:07:54 +0700

    update prefered string to number function

--- string_tonumber/README.md
+++ string_tonumber/README.md
@@ -2,7 +2,15 @@
 ```
 $ make
 $ ./a.out
-1234
-1706.198600
+using_atox(21) 1234
+using_atox(22) 1234
+using_atox(23) 1706.198600
+----------------------
+prefered_converter(35) 1234      size: 4
+prefered_converter(36) 1234      size: 8
+prefered_converter(37) 1706.198608       size: 4
+prefered_converter(38) 1706.198600       size: 8
+prefered_converter(39) 1706.198600       size: 16
+----------------------
 $
 ```
--- string_tonumber/string_tonumber.c
+++ string_tonumber/string_tonumber.c
@@ -10,17 +10,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+const char string_int[] = "1234";
+const char string_float[] = "1706.1986";
+
+void using_atox(){
+	int 		num_int 	= atoi(string_int);
+	long int 	num_long 	= atol(string_int);
+	double 		num_double 	= atof(string_float);
+
+	fprintf(stdout, "%s(%d) %d\n", __FUNCTION__, __LINE__, num_int);
+	fprintf(stdout, "%s(%d) %ld\n", __FUNCTION__, __LINE__, num_long);
+	fprintf(stdout, "%s(%d) %lf\n", __FUNCTION__, __LINE__, num_double);
+
+	return;
+}
+
+void prefered_converter(){
+	int 		num_int 	= (int)strtol(string_int, (char **)NULL, 10);
+	long int 	num_long 	= strtol(string_int, (char **)NULL, 10);
+	float 		num_float 	= strtof(string_float, (char **)NULL);
+	double 		num_double 	= strtod(string_float, (char **)NULL);
+	long double num_ldouble = strtold(string_float, (char **)NULL);
+
+	fprintf(stdout, "%s(%d) %d \t size: %lu\n", __FUNCTION__, __LINE__, num_int, sizeof(num_int));
+	fprintf(stdout, "%s(%d) %ld \t size: %lu\n", __FUNCTION__, __LINE__, num_long, sizeof(num_long));
+	fprintf(stdout, "%s(%d) %f \t size: %lu\n", __FUNCTION__, __LINE__, num_float, sizeof(num_float));	
+	fprintf(stdout, "%s(%d) %lf \t size: %lu\n", __FUNCTION__, __LINE__, num_double, sizeof(num_double));
+	fprintf(stdout, "%s(%d) %Lf \t size: %lu\n", __FUNCTION__, __LINE__, num_ldouble, sizeof(num_ldouble));
+
+	return;
+}
+
 int main(int argc, char * argv[]){
-	char string_int[] = "1234";
-	char string_float[] = "1706.1986";
 
-	int num_int = atoi(string_int);
-	long int num_long = atol(string_int);
-	double num_float = atof(string_float);
+	// Stay away from atox function 
+	// because when the value cannot be represented, handling of errors behavior is undefined.
+	// https://stackoverflow.com/questions/7021725/converting-string-to-integer-c	
+	using_atox();
+	printf("----------------------\n");
 
-	fprintf(stdout, "%d\n", num_int);
-	fprintf(stdout, "%ld\n", num_long);
-	fprintf(stdout, "%f\n", num_float);
+	prefered_converter();
+	printf("----------------------\n");
 
 	return 0;
 }
