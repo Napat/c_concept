5b834c70bef7156ed68319a88e1b3cb267085b08
Napat Rungruangbangchan <napat.joe@gmail.com>
2018-06-23 02:01:57 +0700

    rest api client using libcurl

---
+++ communication/restapiclient/uselibcurl/README.md
@@ -0,0 +1,169 @@
+# Using libcurl to make restapi client
+
+## Install libcurl to system
+
+See [REQUIMENT.md](REQUIMENT.md)
+
+## Let's begin
+
+แม้การเขียนโปรแกรมในช่วงเริ่มต้นเพื่อติดต่อกับ rest API ของ http server ด้วยภาษา C ด้วยการเปิด socket จะทำได้ไม่ยากเย็นอะไรนัก  
+แต่ในระยะยาวๆเมื่อต้องติดต่อกับโปรโตคอลที่หลากหลายมากขึ้น เช่น https, sftp หรือเมื่อมีความต้องการรองรับ header หลายๆรูปแบบแล้ว  
+จะพบว่าเราต้องไปจัดการรายละเอียดอะไรเยอะมากมายเกินกว่าที่เราควรจะต้องไปทำมัน  
+อีกทั้งการที่เราสามารถต่อยอดไปใช้ชุดคำสั่งอื่นๆได้อย่างรวดเร็ว เนื่องจาก library curl ได้เตรียมชุดคำสั่งเยอะแยะมากมายให้เรียกใช้งานได้อย่างง่ายดายกว่าการเขียนเองทั้งหมดอย่างมาก  
+ด้วยเหตผลดังกล่าว การใช้ libcurl บนระบบ Unix/Linux จึงได้รับความนิยมอย่างมาก
+
+นอกจากชุดคำสั่งต่างๆแล้ว curl ก็ยังมี tools ที่อำนวยความสะดวกสำหรับการเขียนโปรแกรมให้เราใช้งานเพิ่มเติม ตัวอย่างเช่น
+
+`curl-config --cflags`: ใช้ตรวจสอบข้อมูลเช่น ที่อยู่ของ header, include path ที่จำเป็นต้องใส่ใน cflag โดยทั่วไปแล้วมักจะว่างเปล่าไม่มีค่าอะไรคืนกลับมา
+
+`curl-config --libs`: ใช้หาว่าแฟลกในขั้นตอนการ linking โปรแกรมเข้ากับ libcurl เช่น `-L/usr/lib/x86_64-linux-gnu -lcurl` เป็นต้น
+
+`curl-config --feature`: ใช้ตรวจสอบฟีเจอร์ต่างๆว่า libcurl ที่ติดตั้งสามารถใช้งานฟีเจอร์อะไรได้บ้าง (ฟีเจอร์ต่างๆอาจจะถูกเปิดเพิ่ม/ปิดออกไปในขั้นตอน configure/make library) เช่น หากไม่ได้เปิด SSL ไว้จะสามารถใช้ http, ftp ได้แต่จะไม่สามารถใช้ https, sftp ได้เป็นต้น
+
+``` bash
+$ curl-config --feature
+SSL
+IPv6
+UnixSockets
+libz
+AsynchDNS
+IDN
+GSS-API
+PSL
+SPNEGO
+Kerberos
+NTLM
+NTLM_WB
+TLS-SRP
+HTTP2
+HTTPS-proxy
+```
+
+`docs/libcurl/libcurl.m4 file`: ในกรณีที่ต้องการสร้าง configure script เพื่อสร้างตัวแปรต่างๆในการเขียนโปรแกรมร่วมกับ libcurl สามารถอ่านวิธีการได้ในไฟล์ดังกล่าว
+
+### thread safety???
+
+ประเด็นหนึ่งที่ต้องสนใจก็คือเรื่อง thread safety ใครจะใช้ libcurl จำเป็นต้องอ่าน > https://curl.haxx.se/libcurl/c/threadsafe.html
+สรุปสั้นๆก็คือ มันมีทั้ง api ที่ thread safety (แต่ก็ยังมีเรื่องที่ว่าต้องสร้าง mutex lock เพิ่มเติมเข้าไป) และแบบไม่ safety (พวก global ทั้งหลาย) ดังนั้นต้อง *เข้าไปอ่านโดยไม่มีข้อแม้*นะครับ
+
+### main.c
+
+``` bash
+$ ./output/respapiclient
+user_q1 0x603090
+user_q2 0x6030b0
+user_q3 0x6030d0
+user_q3 0x6030f0
+curl_get_xN(51) begin----------------------------------------
+user pointer: 0x603090
+        realsize\size\nmemb 66\1\66
+        contents length: 66
+        contents: {
+ "msg": "Hello World. Today is good day",
+ "version": "1.0.0"
+}
+
+user pointer: 0x603090
+        realsize\size\nmemb 66\1\66
+        contents length: 66
+        contents: {
+ "msg": "Hello World. Today is good day",
+ "version": "1.0.0"
+}
+
+user pointer: 0x603090
+        realsize\size\nmemb 66\1\66
+        contents length: 66
+        contents: {
+ "msg": "Hello World. Today is good day",
+ "version": "1.0.0"
+}
+
+curl_get_xN(56) end----------------------------------------
+curl_post_json_xN(87) begin----------------------------------------
+user pointer: 0x6030b0
+        realsize\size\nmemb 46\1\46
+        contents length: 46
+        contents: {
+ "msg": "Hello POST.",
+ "version": "1.0.1"
+}
+user pointer: 0x6030b0
+        realsize\size\nmemb 46\1\46
+        contents length: 46
+        contents: {
+ "msg": "Hello POST.",
+ "version": "1.0.1"
+}
+user pointer: 0x6030b0
+        realsize\size\nmemb 46\1\46
+        contents length: 46
+        contents: {
+ "msg": "Hello POST.",
+ "version": "1.0.1"
+}
+curl_post_json_xN(92) end----------------------------------------
+curl_post_xml_xN(128) begin----------------------------------------
+user pointer: 0x6030d0
+        realsize\size\nmemb 53\1\53
+        contents length: 53
+        contents: {
+ "msg": "Hello POST xml.",
+ "version": "1.0.2"
+}
+user pointer: 0x6030d0
+        realsize\size\nmemb 53\1\53
+        contents length: 53
+        contents: {
+ "msg": "Hello POST xml.",
+ "version": "1.0.2"
+}
+user pointer: 0x6030d0
+        realsize\size\nmemb 53\1\53
+        contents length: 53
+        contents: {
+ "msg": "Hello POST xml.",
+ "version": "1.0.2"
+}
+curl_post_xml_xN(133) end----------------------------------------
+curl_post_appxwwwformurlencoded_xN(172) begin----------------------------------------
+user pointer: 0x6030f0
+        realsize\size\nmemb 82\1\82
+        contents length: 82
+        contents: {
+ "msg": "tasks_post-x-www-form-urlencoded/v1/compareface",
+ "version": "1.0.3"
+}
+user pointer: 0x6030f0
+        realsize\size\nmemb 82\1\82
+        contents length: 82
+        contents: {
+ "msg": "tasks_post-x-www-form-urlencoded/v1/compareface",
+ "version": "1.0.3"
+}
+user pointer: 0x6030f0
+        realsize\size\nmemb 82\1\82
+        contents length: 82
+        contents: {
+ "msg": "tasks_post-x-www-form-urlencoded/v1/compareface",
+ "version": "1.0.3"
+}
+curl_post_appxwwwformurlencoded_xN(177) end----------------------------------------
+$
+```
+
+
+## References
+
+- [libcurl c api document](https://curl.haxx.se/libcurl/c/)
+- [libcurl github](https://github.com/curl/curl/blob/master/docs/INSTALL.md)
+- [libcurl example: getinmemory](https://curl.haxx.se/libcurl/c/getinmemory.html)
+- [libcurl tutorial](https://curl.haxx.se/libcurl/c/libcurl-tutorial.html)
+- [restful tutorial](http://www.drdobbs.com/web-development/restful-web-services-a-tutorial/240169069)
+
+## Userful
+- [mocky rest api server support both http/https](https://www.mocky.io/)
+- [mockable rest api](https://www.mockable.io/)
+
+
+
---
+++ communication/restapiclient/uselibcurl/main.c
@@ -0,0 +1,199 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <curl/curl.h>
+
+char user_q1[20] = {};
+char user_q2[20] = {};
+char user_q3[20] = {};
+char user_q4[20] = {};
+
+size_t curl_cbfn(void *contents, size_t size, size_t nmemb, void *userp) {
+    size_t realsize = size * nmemb;
+
+    printf("user pointer: %p\n", userp);
+    printf("\trealsize\\size\\nmemb %lu\\%lu\\%lu\n", realsize, size, nmemb);
+    printf("\tcontents length: %lu\n", strlen((char *)contents));
+    printf("\tcontents: %s\n", (char *)contents);
+
+    return realsize;
+}
+
+int curl_error(CURLcode err) {
+    // More about CURLcode: https://curl.haxx.se/libcurl/c/libcurl-errors.html
+    if (err != CURLE_OK) {
+        printf("%s(%d) CURLE_xxx: error %d\n", __FUNCTION__, __LINE__, err);
+        return err;
+    }
+    //printf("%s(%d) CURLE_OK: All fine. Proceed as usual\n", __FUNCTION__, __LINE__);
+    return 0;
+}
+
+int curl_get_xN() {
+    int loop, xN = 3;
+    CURL *curl_easy;
+    CURLcode res;
+    char url[] = "http://demo6460359.mockable.io/tasks_get/v1/users?name=napat&lastname=roo";
+    curl_easy = curl_easy_init();
+    if (curl_easy == NULL) {
+        printf("%s(%d) curl_easy_init error: something went wrong %p\n", __FUNCTION__, __LINE__, curl_easy);
+        return -1;
+    }
+    // set callback fn
+    curl_easy_setopt(curl_easy, CURLOPT_WRITEFUNCTION, curl_cbfn);
+    // set user pointer pass to callback fn
+    curl_easy_setopt(curl_easy, CURLOPT_WRITEDATA, (void *)user_q1);
+    // set url
+    curl_easy_setopt(curl_easy, CURLOPT_URL, url);
+    // set http_get
+    curl_easy_setopt(curl_easy, CURLOPT_HTTPGET, 1L);
+
+    printf("%s(%d) begin----------------------------------------\n", __FUNCTION__, __LINE__);
+    for(loop = 0; loop < xN; loop++) {
+        // get away #n
+        res = curl_easy_perform(curl_easy);
+    }
+    printf("%s(%d) end----------------------------------------\n", __FUNCTION__, __LINE__);
+
+    // clean: MUST CALL cleanup when use curl_easy_init()
+    curl_easy_cleanup(curl_easy);
+
+    return curl_error(res);
+}
+
+int curl_post_json_xN() {
+    int loop, xN = 3;
+    CURL *curl_easy;
+    CURLcode res;
+    char url[] = "http://demo6460359.mockable.io/tasks_postjson/v1/users";
+    char post_data[] = "username=newuser&password=newpasswd&msg=test&msisdn=9999999999&tagname=Demo&shortcode=8888&telcoId=5&dnRequired=false";
+    curl_easy = curl_easy_init();
+    if (curl_easy == NULL) {
+        printf("%s(%d) curl_easy_init error: something went wrong %p\n", __FUNCTION__, __LINE__, curl_easy);
+        return -1;
+    }
+
+    // set callback fn
+    curl_easy_setopt(curl_easy, CURLOPT_WRITEFUNCTION, curl_cbfn);
+    // set user pointer pass to callback fn
+    curl_easy_setopt(curl_easy, CURLOPT_WRITEDATA, (void *)user_q2);
+    // set url
+    curl_easy_setopt(curl_easy, CURLOPT_URL, url);
+    // post data to the server without having libcurl do a default strlen() to measure the data size
+    curl_easy_setopt(curl_easy, CURLOPT_POSTFIELDSIZE, (long)strlen(post_data));
+    // set data to post
+    curl_easy_setopt(curl_easy, CURLOPT_POSTFIELDS, post_data);
+
+    printf("%s(%d) begin----------------------------------------\n", __FUNCTION__, __LINE__);
+    for (loop = 0; loop < xN; loop++) {
+        // post away
+        res = curl_easy_perform(curl_easy);
+    }
+    printf("%s(%d) end----------------------------------------\n", __FUNCTION__, __LINE__);
+
+    // clean: MUST CALL cleanup when use curl_easy_init()
+    curl_easy_cleanup(curl_easy);
+
+    return curl_error(res);
+}
+
+int curl_post_xml_xN() {
+    int loop, xN = 3;
+    CURL *curl_easy;
+    CURLcode res;
+    char url[] = "http://demo6460359.mockable.io/tasks_postxml/v1/users";
+    char post_data[] = "<?xml version=\"1.0\" encoding=\"utf-8\"?><Person><ID>1</ID><Name>Ana Conda</Name><Email>a.conda@gmail.com</Email><Country>India</Country></Person>";
+    struct curl_slist *headers = NULL;    
+
+    curl_easy = curl_easy_init();
+    if (curl_easy == NULL) {
+        printf("%s(%d) curl_easy_init error: something went wrong %p\n", __FUNCTION__, __LINE__, curl_easy);
+        return -1;
+    }
+
+    // set header
+    headers = curl_slist_append(headers, "Content-Type: text/xml");    
+    curl_easy_setopt(curl_easy, CURLOPT_HTTPHEADER, headers);
+    // set callback fn
+    curl_easy_setopt(curl_easy, CURLOPT_WRITEFUNCTION, curl_cbfn);
+    // set user pointer pass to callback fn
+    curl_easy_setopt(curl_easy, CURLOPT_WRITEDATA, (void *)user_q3);
+    // set url
+    curl_easy_setopt(curl_easy, CURLOPT_URL, url);
+    // post data to the server without having libcurl do a default strlen() to measure the data size
+    curl_easy_setopt(curl_easy, CURLOPT_POSTFIELDSIZE, (long)strlen(post_data));
+    // set data to post
+    curl_easy_setopt(curl_easy, CURLOPT_POSTFIELDS, post_data);
+
+    printf("%s(%d) begin----------------------------------------\n", __FUNCTION__, __LINE__);
+    for (loop = 0; loop < xN; loop++) {
+        // post away
+        res = curl_easy_perform(curl_easy);
+    }
+    printf("%s(%d) end----------------------------------------\n", __FUNCTION__, __LINE__);
+
+    // clean header
+    curl_slist_free_all(headers);
+    // clean: MUST CALL cleanup when use curl_easy_init()
+    curl_easy_cleanup(curl_easy);
+
+    return curl_error(res);
+}
+
+int curl_post_appxwwwformurlencoded_xN() {
+    int loop, xN = 3;
+    CURL *curl_easy;
+    CURLcode res;
+    char url[] = "http://demo6460359.mockable.io/tasks_post-x-www-form-urlencoded/v1/compareface";
+    char post_data[] = "{\"img1base64\":\"xxxxxx\",\"img2base64\":\"yyyyyy\"}";
+    struct curl_slist *headers = NULL;
+
+    curl_easy = curl_easy_init();
+    if (curl_easy == NULL) {
+        printf("%s(%d) curl_easy_init error: something went wrong %p\n", __FUNCTION__, __LINE__, curl_easy);
+        return -1;
+    }
+
+    // set header
+    headers = curl_slist_append(headers, "Content-Type: application/x-www-form-urlencoded");
+    curl_easy_setopt(curl_easy, CURLOPT_HTTPHEADER, headers);
+    // set callback fn
+    curl_easy_setopt(curl_easy, CURLOPT_WRITEFUNCTION, curl_cbfn);
+    // set user pointer pass to callback fn
+    curl_easy_setopt(curl_easy, CURLOPT_WRITEDATA, (void *)user_q4);
+    // set url
+    curl_easy_setopt(curl_easy, CURLOPT_URL, url);
+    // post data to the server without having libcurl do a default strlen() to measure the data size
+    curl_easy_setopt(curl_easy, CURLOPT_POSTFIELDSIZE, (long)strlen(post_data));
+    // set data to post
+    curl_easy_setopt(curl_easy, CURLOPT_POSTFIELDS, post_data);
+
+    printf("%s(%d) begin----------------------------------------\n", __FUNCTION__, __LINE__);
+    for (loop = 0; loop < xN; loop++) {
+        // post away
+        res = curl_easy_perform(curl_easy);
+    }
+    printf("%s(%d) end----------------------------------------\n", __FUNCTION__, __LINE__);
+
+    // clean header
+    curl_slist_free_all(headers);
+    // clean: MUST CALL cleanup when use curl_easy_init()
+    curl_easy_cleanup(curl_easy);
+
+    return curl_error(res);
+}
+
+int main(int argc, char *argv[]) {
+    curl_global_init(CURL_GLOBAL_ALL);
+
+    printf("user_q1 %p\n", user_q1);
+    printf("user_q2 %p\n", user_q2);
+    printf("user_q3 %p\n", user_q3);
+    printf("user_q3 %p\n", user_q4);
+
+    curl_get_xN();
+    curl_post_json_xN();
+    curl_post_xml_xN();
+    curl_post_appxwwwformurlencoded_xN();
+    return 0;
+}
